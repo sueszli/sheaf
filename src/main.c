c90ce760f607578aaf4fda9a4f72057bc627c420
sueszli <jabaryyahya@gmail.com>
2025-09-16 02:56:45 +0200

    up

--- src/main.c
+++ src/main.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 
 int main() {
-  printf("Hello, world!\n");
-
-  // Intentional memory leak for testing
-  char *leaked_memory = malloc(100);
-  if (leaked_memory) {
-    sprintf(leaked_memory, "This memory will leak!");
-    printf("Allocated memory: %s\n", leaked_memory);
-    // Note: deliberately not calling free(leaked_memory)
-  }
-
-  return 0;
+    printf("Hello, world!\n");
+    return 0;
 }
